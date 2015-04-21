77610ac724aa031b6244df3f83e88ee588fd911c
Alexander Hill <alexreedhill@gmail.com>
2015-04-21 12:53:10 -0700

    lein new

---
+++ spec/clojure_om_ttt/core_spec.clj
@@ -0,0 +1,7 @@
+(ns clojure-om-ttt.core-spec
+  (:require [speclj.core :refer :all]
+            [clojure-om-ttt.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ src/clj/clojure_om_ttt/core.clj
@@ -0,0 +1,1 @@
+(ns clojure-om-ttt.core)
