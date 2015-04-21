07613ae00e28e6c44d14c09f2400a2945b5ece9e
Alexander Hill <alexreedhill@gmail.com>
2015-04-21 13:00:44 -0700

    include ClojureScript, Om

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.2.0"]]}}
+  :profiles {:dev {:dependencies [[speclj "3.2.0"]
+                                  [org.clojure/clojurescript "0.0-3196"]
+                                  [org.omcljs/om "0.8.8"]]}}
   :plugins [[speclj "3.2.0"]]
   :test-paths ["spec"])
