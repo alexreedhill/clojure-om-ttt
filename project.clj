ec1016d2ed9e653004fee64d4eaf6cc737a7944d
Alexander Hill <alexreedhill@gmail.com>
2015-04-26 12:06:55 -0700

    clojurescript, om, and numeric-tower are depended on in all envs

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.2.0"]
-                                  [org.clojure/clojurescript "0.0-3196"]
-                                  [org.omcljs/om "0.8.8"]
-                                  [org.clojure/math.numeric-tower "0.0.4"]]}}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-3196"]
+                 [org.omcljs/om "0.8.8"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :profiles {:dev {:dependencies [[speclj "3.2.0"]]}}
   :plugins [[speclj "3.2.0"]]
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["spec/clj", "spec/cljs"])
