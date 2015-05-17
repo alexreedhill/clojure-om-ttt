34cf162bd7a955c0c3fd5796dd9162678aa84862
Alexander Hill <alexreedhill@gmail.com>
2015-05-17 13:18:10 -0500

    fill in 4x4 boards in ai spec more for faster tests

--- spec/clj/om_ttt/players/ai_spec.clj
+++ spec/clj/om_ttt/players/ai_spec.clj
@@ -61,58 +61,58 @@
 
     (context "4x4"
       (it "blocks row"
-        (should= [nil nil nil nil
+        (should= ["O" "X" "O" "X"
                   "O" "O" "O" "X"
                   "X" nil nil nil
-                  nil nil nil nil]  (make-move ai-player-x [nil nil nil nil
+                  "X" "O" "X" nil]  (make-move ai-player-x ["O" "X" "O" "X"
                                                             "O" "O" "O" nil
                                                             "X" nil nil nil
-                                                            nil nil nil nil])))
+                                                            "X" "O" "X" nil])))
 
       (it "blocks column"
         (should= [nil "X" nil nil
-                  nil "O" nil nil
-                  "X" "O" nil nil
-                  nil "O" nil nil] (make-move ai-player-x [nil nil nil nil
-                                                           nil "O" nil nil
-                                                           "X" "O" nil nil
-                                                           nil "O" nil nil])))
+                  "X" "O" "X" "X"
+                  "X" "O" "X" "O"
+                  "O" "O" "O" "X"] (make-move ai-player-x [nil nil nil nil
+                                                           "X" "O" "X" "X"
+                                                           "X" "O" "X" "O"
+                                                           "O" "O" "O" "X"])))
 
       (it "blocks diagonal"
-        (should= ["X" nil nil nil
-                  nil "O" nil nil
-                  "X" nil "O" nil
-                  nil nil nil "O"] (make-move ai-player-x [nil nil nil nil
-                                                           nil "O" nil nil
-                                                           "X" nil "O" nil
-                                                           nil nil nil "O"])))
+        (should= ["O" nil "O" nil
+                  "X" "X" "X" "O"
+                  "X" "O" "X" "O"
+                  "O" "O" "O" "X"] (make-move ai-player-o [nil nil "O" nil
+                                                           "X" "X" "X" "O"
+                                                           "X" "O" "X" "O"
+                                                           "O" "O" "O" "X"])))
 
       (it "wins row"
         (should= ["X" nil nil nil
                   "X" "X" nil nil
                   "O" "O" "O" "O"
-                  nil nil nil nil] (make-move ai-player-o ["X" nil nil nil
+                  nil "X" "X" "X"]  (make-move ai-player-o ["X" nil nil nil
                                                            "X" "X" nil nil
                                                            nil "O" "O" "O"
-                                                           nil nil nil nil])))
+                                                           nil "X" "X" "X"])))
 
       (it "wins column"
         (should= ["X" nil nil nil
-                  "X" "O" nil nil
+                  "X" "O" nil "O"
                   "X" "O" "O" nil
-                  "X" nil nil nil] (make-move ai-player-x [nil nil nil nil
-                                                           "X" "O" nil nil
+                  "X" "O" "X" nil] (make-move ai-player-x [nil nil nil nil
+                                                           "X" "O" nil "O"
                                                            "X" "O" "O" nil
-                                                           "X" nil nil nil])))
+                                                           "X" "O" "X" nil])))
 
       (it "wins diagonal"
-          (should= ["O" nil nil nil
+          (should= ["O" nil "X" "X"
                     "X" "O" nil nil
-                    "X" nil "O" nil
-                    nil nil nil "O"] (make-move ai-player-o ["O" nil nil nil
+                    "X" "X" "O" nil
+                    nil "X" "O" "O"] (make-move ai-player-o [nil nil "X" "X"
                                                              "X" "O" nil nil
-                                                             "X" nil "O" nil
-                                                             nil nil nil nil])))
+                                                             "X" "X" "O" nil
+                                                             nil "X" "O" "O"])))
       (it "blocks fork"
         (should-contain
           (make-move ai-player-o
