acb069d2aa9bb1450d69f02d194aa84daf600c7d
Alexander Hill <alexreedhill@gmail.com>
2015-05-17 21:36:13 -0500

    cut down game loop for better adherence to SRP

--- spec/clj/om_ttt/game_loop_spec.clj
+++ spec/clj/om_ttt/game_loop_spec.clj
@@ -4,15 +4,14 @@
             [om-ttt.players.ai :refer [new-ai-player]]
             [om-ttt.players.human :refer [new-human-player]]
             [om-ttt.protocols.ui :as ui]
-            [om-ttt.spec-helper :refer [mock-ui]]
+            [om-ttt.spec-helper :refer [empty-board mock-ui]]
             [speclj.core :refer :all]))
 
 (def ai-token "X")
 (def human-token "O")
-(def config {:first-player "ai" :ai-token ai-token :human-token human-token :board-size 3})
 (def ai-player (new-ai-player ai-token human-token))
 (def mock-human-player (new-human-player human-token mock-ui))
 
 (describe "game"
   (it "plays several games all the way through"
-    (should= ai-token (r/winner (start-game config ai-player mock-human-player mock-ui)))))
+    (should= ai-token (r/winner (start-game empty-board [ai-player mock-human-player] mock-ui)))))
--- src/clj/om_ttt/game_loop.clj
+++ src/clj/om_ttt/game_loop.clj
@@ -1,33 +1,10 @@
 (ns om-ttt.game-loop
   (:require [om-ttt.protocols.ui :as ui]
             [om-ttt.protocols.player :as player]
-            [om-ttt.board :as b]
             [om-ttt.rules :as r]))
 
-(declare start-game)
-
-(defn- new-game []
-  )
-
-(defn- game-over [config players board ui]
-  (do
-    (ui/display-message ui (str "Game over! " (r/winner board) " won. Would you like to play again?"))
-    (if (ui/restart? ui)
-      (if (ui/same-config? ui)
-        (start-game config (first players) (second players) ui)
-        (new-game ui))
-      board)))
-
-(defn- game-loop [config players board ui]
+(defn start-game [board players ui]
   (if (r/game-over? board)
-    (game-over config players board ui)
-    (recur config (reverse players) (player/make-move (first players) board) ui)))
-
-(defn- order-players [config ai-player human-player]
-  (if (= "ai" (config :first-player))
-    [ai-player human-player]
-    [human-player ai-player]))
-
-(defn start-game [config ai-player human-player ui]
-  (let [players (order-players config ai-player human-player)]
-    (game-loop config players (b/generate (config :board-size)) ui)))
+    board
+    (-> (player/make-move (first players) board)
+        (recur (reverse players) ui))))
