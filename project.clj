a541d107c11a612927b14f8d43bbcb5b563a4409
Alexander Hill <alexreedhill@gmail.com>
2015-07-26 15:26:46 -0400

    clojure 1.7, migrate shared source to cljc, test cljs with phantomjs

--- project.clj
+++ project.clj
@@ -3,12 +3,36 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3196"]
-                 [org.omcljs/om "0.8.8"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "0.0-2760"]
+                 [com.cemerick/clojurescript.test "0.3.3"]
+                 [org.omcljs/om "0.9.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [prismatic/dommy "1.1.0"]]
   :profiles {:dev {:dependencies [[speclj "3.2.0"]]}}
-  :plugins [[speclj "3.2.0"]]
-  :source-paths ["src/clj" "src/cljs"]
+
+  :plugins [[speclj "3.2.0"]
+            [lein-cljsbuild "1.0.5"]
+            [com.cemerick/clojurescript.test "0.3.3"]]
+
+  :source-paths ["src/clj" "src/cljs", "src/cljc"]
   :test-paths ["spec/clj", "spec/cljs"]
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src"]
+              :compiler {:output-to "resources/public/app.js"
+                         :output-dir "out"
+                         :optimizations :whitespace
+                         :pretty-print true}}
+
+             {:id "tests"
+                :source-paths ["src/cljs" "spec/cljs"]
+                :compiler {:pretty-print true
+                           :output-dir "resources/private/js"
+                           :output-to "resources/private/js/unit-test.js"
+                           :optimizations :whitespace }}]
+
+    :test-commands {"unit-tests" ["phantomjs" :runner
+                            "resources/private/js/phantom/bind-polyfill.js"
+                            "resources/private/js/unit-test.js"]}}
   :main om-ttt.core)
---
+++ src/cljs/om_ttt/core.cljs
@@ -0,0 +1,5 @@
+(ns om-ttt.core
+  (:require [om.core :as om]
+            [om.dom :as dom]))
+
+
