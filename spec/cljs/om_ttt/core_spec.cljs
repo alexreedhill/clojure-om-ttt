660214c0569db614048b6c9b3a804d35e3bf1566
Alexander Hill <alexreedhill@gmail.com>
2015-07-26 17:51:55 -0400

    use fixtures for app setup in tests, ensure ui displays cell values

--- spec/cljs/om_ttt/core_spec.cljs
+++ spec/cljs/om_ttt/core_spec.cljs
@@ -1,5 +1,5 @@
 (ns om-ttt.cljs.core-spec
-  (:require-macros [cemerick.cljs.test :refer (deftest is testing)]
+  (:require-macros [cemerick.cljs.test :refer (deftest is testing use-fixtures)]
                    [dommy.core :refer (sel sel1)])
   (:require [om-ttt.core :as core :refer [app-state draw-board]]
             [cemerick.cljs.test]
@@ -13,13 +13,19 @@
 (defn append-node [node]
   (dommy/append! (sel1 js/document :body) node))
 
-(defn setup-app-fixture! []
-  (->> (new-node "app") (append-node)))
+(defn app-fixture [f]
+  (->> (new-node "app") (append-node))
+  (f))
 
-(deftest test-draw-board
-  (testing "draws empty board"
-    (setup-app-fixture!)
-    (swap! app-state assoc :board (repeat 9 nil))
-    (draw-board)
-    (is (sel1 :#board))
-    (is (= (count (sel :.cell)) (count (:board @app-state))))))
+(use-fixtures :once app-fixture)
+
+(deftest test-draw-empty-board
+  (swap! app-state assoc :board (repeat 9 nil))
+  (draw-board)
+  (is (sel1 :#board))
+  (is (= (count (sel :.cell)) (count (:board @app-state)))))
+
+(deftest test-draw-board-with-cell-values
+  (swap! app-state assoc :board ["X" "O"])
+  (is (= (dommy/text (first (sel "li")))) "X")
+  (is (= (dommy/text (second (sel "li")))) "O"))
